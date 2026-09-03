b19b3ab674c52046432257ed9d84557e75962215
jacy0917 <155806415+jacy0917@users.noreply.github.com>
2026-09-03 15:27:53 +0800

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,22 +1,27 @@
-import Flutter
 import UIKit
+import Flutter
 
-@main
-@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+    
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let barcodeChannel = FlutterMethodChannel(name: "app.packingproof.mobile/barcode",
+                                              binaryMessenger: controller.binaryMessenger)
+    
+    barcodeChannel.setMethodCallHandler({
+      (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+      if call.method == "useVisionScanner" {
+        result(true)
+      } else {
+        result(FlutterMethodNotImplemented)
+      }
+    })
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
-    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
-    PigeonPlatform.register(with: engineBridge.pluginRegistry)
-  }
-
-  override func applicationWillTerminate(_ application: UIApplication) {
-    PigeonPlatform.shutdownForTermination()
-    super.applicationWillTerminate(application)
   }
 }
